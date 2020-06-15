cadc739420c1eb492ab4bfd19bb4941105534c2b
fishb <18870033089@163.com>
2020-06-15 14:07:29 +0800

    提前24小时预订

--- src/view/index.vue
+++ src/view/index.vue
@@ -21,7 +21,7 @@
     <time-line domRef="vs3" :minHour="7" :maxHour="23" :checkedList="[12,12.5,13.5]"  @getHours="getHours" :clear="clear=='vs3'" @clearHours="clearHours"/>
     <p>其它人选择过的时段</p>
     <time-line domRef="vs18" :minHour="10.5" :maxHour="21.5" :disabledList="[{name:'张三',time:15},{name:'李四',time:15.5},{name:'王五',time:16}]" @getHours="getHours" :clear="clear=='vs18'" @clearHours="clearHours"/>
-    <p>外部传来的时间根据日期预订</p>
+    <p>外部传来的时间根据日期预订（需要提前24小时预订）</p>
     <time-line domRef="vs19" :date="formData.time" :advance="24" :minHour="7" :maxHour="23" @getHours="getHours" :clear="clear=='vs19'" @clearHours="clearHours"/>
   </div>
 </template>
