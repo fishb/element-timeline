91e0412917ceee4b8b3d069028842ac952c2ee97
fishb <18870033089@163.com>
2023-02-28 00:07:06 +0800

    在线预览地址

---
+++ config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"http://localhost:8080"' // 要配置本地地址，配置后台地址不能访问
+})
